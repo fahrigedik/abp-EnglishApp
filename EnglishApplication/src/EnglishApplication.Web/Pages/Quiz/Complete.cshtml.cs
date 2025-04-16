bf18d5dbc23b19e5612595d409bd4afc7f3b6bdb
Fahri Gedik <fahrican9978@gmail.com>
2025-04-16 11:03:57 +0300

    create main page

---
+++ EnglishApplication/src/EnglishApplication.Web/Pages/Quiz/Complete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnglishApplication.Web.Pages.Quiz
+{
+    public class CompleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
